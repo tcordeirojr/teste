333db36ecb4d9d9c117e0f565ce3f85eee44b927
tcordeirojr <tedcordeirojr@gmail.com>
2025-09-24 12:02:56 -0300

    update 1

--- teste/vite.config.js
+++ teste/vite.config.js
@@ -1,23 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://SEU-PROJETO.vercel.app',
-        changeOrigin: true,
-        secure: true,
-      }
-    }
-  }
 })
